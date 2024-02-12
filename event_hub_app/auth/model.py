71c444936c2e095eb6ab65cdff869a43258291e0
Manikanth <amanikanth@deloitte.com>
2024-02-13 01:43:30 +0530

    updated the event database with duration field

--- event_hub_app/auth/model.py
+++ event_hub_app/auth/model.py
@@ -1,9 +1,9 @@
 from datetime import datetime
+from datetime import timedelta
 from flask_login import UserMixin
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
-
 
 
 @login_manager.user_loader
@@ -18,7 +18,7 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     __tablename__ = "users"
 
-    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(256), unique=True, nullable=False)
     phone_number = db.Column(db.String(10), nullable=False)
@@ -39,19 +39,23 @@ class User(db.Model, UserMixin):
 class Event(db.Model):
     __tablename__ = 'events'
 
-    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    duration = db.Column(db.String(20), nullable=False)
     location = db.Column(db.String(100))
     organizer_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
     feedback = db.relationship('Feedback', backref='event', lazy=True)
     participants = db.relationship('User', secondary='event_participant', backref='attended_events')
 
-    def __init__(self, name, description, datetime, location, organizer_id):
+    def __init__(self, name, description, date_time, event_duration, location, organizer_id):
         self.name = name
         self.description = description
-        self.datetime = datetime
+        self.date_time = date_time
+        self.duration = event_duration
         self.location = location
         self.organizer_id = organizer_id
 
@@ -62,11 +66,14 @@ class Event(db.Model):
 class Feedback(db.Model):
     __tablename__ = 'feedbacks'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, default=lambda: str(uuid.uuid4()))
     event_id = db.Column(db.String(36), db.ForeignKey('events.id'), nullable=False)
     comment = db.Column(db.Text)
     rating = db.Column(db.Integer)
 
+    def __init__(self, rating, comment):
+        self.rating = rating
+        self.comment = comment
 
 event_participant = db.Table(
     'event_participant',
